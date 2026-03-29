c9a80b1af5687b15d55329a8421251d4ae72c6b6
AverageProgrammer23 <sebastianjvalenzuelavargas@gmail.com>
2026-03-29 00:32:03 -0400

    Version 2 massive upgrade wallahi

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 const reveals = document.querySelectorAll(".reveal");
 
-const observer = new IntersectionObserver(
-  entries => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add("show");
-      } else {
-        entry.target.classList.remove("show");
-      }
-    });
-  },
-  { threshold: 0.15 }
-);
+if ("IntersectionObserver" in window) {
+  const observer = new IntersectionObserver(
+    entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+        } else {
+          entry.target.classList.remove("show");
+        }
+      });
+    },
+    { threshold: 0.15 }
+  );
 
-reveals.forEach(el => observer.observe(el));
+  reveals.forEach(el => observer.observe(el));
+} else {
+  reveals.forEach(el => el.classList.add("show"));
+}
